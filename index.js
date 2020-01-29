d9f164269fa2de7268a2bea87410d6fb9a71906d
Cody Denniston <dennistonc4817@gapps.bcsds.org>
2020-01-29 10:19:41 -0600

    work

--- index.js
+++ index.js
@@ -160,8 +160,13 @@ function processContains(item, list, callback) {
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+function processDuplicateFree(list, callback) {
+   
+    let newList = new Set(list);
+    let newListArray = [...newList]
+    return callback(newListArray);
+  
+  
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
